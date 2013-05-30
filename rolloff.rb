71db9cdbe885fe6f8b3f2e06a5eafe5875c1c93c
Dave Brown <pair+dave@howaboutwe.com>
2013-05-30 13:05:41 -0400

    Fixed as much of rolloff as possible. Removed Rolloff from active plugin list.

--- rolloff.rb
+++ rolloff.rb
@@ -22,22 +22,27 @@ class Rolloff < CampfireBot::Plugin
   def on_rolloff(msg)
     return if @fiber && @fiber.alive?
 
-    @limit = m[:message].strip.to_i
+    @limit = msg[:message].strip.to_i
     @limit    = SIDES_OF_A_DIE if @limit < 2
     @attempts = {}
 
     @fiber = Fiber.new do
       sleep 10
 
-      high_score = @attempts.sort_by { |nick, roll| roll }.last.last
-      @winners   = @attempts.select { |nick, roll| roll  == high_score }
+      if @attempts.any?
+        high_score = @attempts.sort_by { |nick, roll| roll }.last.last
+        @winners   = @attempts.select { |nick, roll| roll  == high_score }
+      end
     end
 
     @fiber.resume
 
-    winning_names = @winners.map(&:first)
+    winning_names = []
+    winning_names = @winners.map(&:first) if @winners
 
-    if winning_names.length == 1
+    if winning_names.empty?
+      msg.speak "LAME: No one guessed!!!!111111one"
+    elsif winning_names.length == 1
       msg.speak "W1NN4R: #{winning_names.first}!!!111one"
     else
       msg.speak "TIE!!: #{winning_names.join(", ")}!!!111one"
