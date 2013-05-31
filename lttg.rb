df782ba71b298c55657de64bd140c2aa1ef37f05
Dave Brown <pair+dave@howaboutwe.com>
2013-05-31 17:36:01 -0400

    Added LTTG

---
+++ lttg.rb
@@ -0,0 +1,11 @@
+class Lttg < CampfireBot::Plugin
+  on_command 'LTTG', :on_lttg
+
+  def lttg(msg)
+    other_user = m[:message].strip
+    if other_user.present?
+      msg.speak "#{other_user}...you are LATE to the mutha-f'in GAAAAAMMME!!!!!!!!!!!!!!!111111111one"
+    end
+  end
+
+end
