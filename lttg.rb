ce3c6b421ebcbf30707af05caed6fdb282cd3641
Dave Brown <pair+dave@howaboutwe.com>
2013-05-31 17:40:38 -0400

    LTTG take 2.

--- lttg.rb
+++ lttg.rb
@@ -1,7 +1,7 @@
 class Lttg < CampfireBot::Plugin
   on_command 'LTTG', :on_lttg
 
-  def lttg(msg)
+  def on_lttg(msg)
     other_user = m[:message].strip
     if other_user.present?
       msg.speak "#{other_user}...you are LATE to the mutha-f'in GAAAAAMMME!!!!!!!!!!!!!!!111111111one"
