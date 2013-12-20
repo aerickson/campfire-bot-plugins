8cd4aa73aef70ffa41d8d28d0ed3b7767287801f
Andrew Erickson <aje@ticketfly.com>
2013-12-19 17:12:53 -0800

    lolcats: fix for site changes

--- lolcats.rb
+++ lolcats.rb
@@ -1,17 +1,20 @@
-require 'open-uri'
-require 'hpricot'
-require 'tempfile'
+require 'mechanize'
 
 class LolCats < CampfireBot::Plugin
   on_command 'lolcat', :lolcats
-  
+
   def initialize
     @log = Logging.logger["CampfireBot::Plugin::Lolcat"]
   end
-  
+
   def lolcats(msg)
     # Scrape random lolcat
-    lolcat = (Hpricot(open('http://icanhascheezburger.com/?random#top'))/'div.entry img').first['src']
+    # BUSTED
+    # lolcat = (Hpricot(open('http://icanhascheezburger.com/?random#top'))/'div.entry img').first['src']
+    # working
+    agent = Mechanize.new
+    urls = agent.get('http://icanhas.cheezburger.com/lolcats').search("//img[contains(@src,'maxW500')]")
+    lolcat = urls[rand(urls.length)]['src'] + 'image.jpg'
     msg.speak(lolcat)
   end
 end
